da015059fe8d9c2b0b8e5ae7e6c7d0189d87d714
Jared Watson <jjaredwatson@gmail.com>
2017-06-05 14:37:41 -0500

    added albums, created cards, and created search

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,3 +3,29 @@
  * This is your main angular file. Edit as you see fit.
  *
  */
+
+ angular
+  .module('tunely', [])
+  .controller('AlbumsIndexController', AlbumsIndexController);
+
+  function AlbumsIndexController () {
+    var vm = this;
+    vm.newAlbum = {};
+
+    vm.albums = [{
+      name: 'License to Ill',
+      artistName: 'Beastie Boys'
+    },
+    {
+      name: 'Coming Home',
+      artistName: 'Leon Bridges'
+    },
+    {
+      name: 'Are We There',
+      artistName: 'Sharon Van Etten'
+    },
+    {
+      name: 'The Queen is Dead',
+      artistName: 'The Smiths'
+    }]
+  }
